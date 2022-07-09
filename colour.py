18ef538e295a471fa662c4ba54b95b4b5469b599
MarcelloBB <marcellobelanda@gmail.com>
2022-07-09 12:11:55 -0300

    :sparkles: Finish colour.py

---
+++ colour.py
@@ -0,0 +1,11 @@
+import colorama
+
+class Colour:
+    colorama.init()
+
+    yellow = colorama.Fore.YELLOW
+    blue = colorama.Fore.BLUE
+    cyan = colorama.Fore.CYAN
+    red = colorama.Fore.RED
+    white = colorama.Fore.WHITE
+    resetStyle = colorama.Style.RESET_ALL
