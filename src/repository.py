6512af7e57e60111627a43b349ea0d6a68c5d433
MarcelloBB <marcellobelanda@gmail.com>
2022-07-09 13:43:52 -0300

    :sparkles: Add TYPES to repository

--- src/repository.py
+++ src/repository.py
@@ -2,18 +2,18 @@ import requests
 import json
 from colour import Colour
 
-def showRepository(repositoryName): print(f"{Colour.cyan}[REPOSITORY] {Colour.blue}{repositoryName}")
+def showRepository(repositoryName: str) -> None: print(f"{Colour.cyan}[REPOSITORY] {Colour.blue}{repositoryName}")
 
 class Repository:
-    def __init__(self, user): self.user = user
+    def __init__(self, user: str) -> None: self.user = user
 
-    def __request(self):
+    def __request(self) -> str:
         requestAddress = f"https://api.github.com/users/{self.user}/repos"
         response = requests.get(requestAddress)
 
         return response.json()
     
-    def doList(self):
+    def doList(self) -> None:
         response = self.__request()
         numberOfRepositories = range(len(response))
 
