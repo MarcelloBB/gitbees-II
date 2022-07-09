6ad235f3f5fb1ef4b884df9a5cb4d4ea382620ac
MarcelloBB <marcellobelanda@gmail.com>
2022-07-09 13:36:59 -0300

    :wrench: Configure gitignore and move colour.py to src

---
+++ src/repository.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from colour import Colour
+
+def showRepository(repositoryName): print(f"{Colour.cyan}[REPOSITORY] {repositoryName}")
+
+class Repository:
+    def __init__(self, user): self.user = user
+
+    def __request(self):
+        requestAddress = f"https://api.github.com/users/{self.user}/repos"
+        response = requests.get(requestAddress)
+
+        return response.json()
+    
+    def doList(self):
+        response = self.__request()
+        numberOfRepositories = range(len(response))
+
+        for repository in numberOfRepositories:
+            repositoryAddress = response[repository]['name']
+            showRepository(repositoryAddress)
+    
+
+r = Repository("MarcelloBB")
+r.doList()
